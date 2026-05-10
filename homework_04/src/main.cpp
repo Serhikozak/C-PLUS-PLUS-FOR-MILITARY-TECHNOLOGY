8b89f1979f7b1b2fc53ff166b34aed323452231c
Serhikozak <skozakov924@gmail.com>
2026-05-10 13:30:46 +0000

    .devcontainer/apt-packages.in

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -8,13 +8,17 @@ int main(int argc, char** argv) {
 
     // TODO: implement wheel odometry for a 4-wheel differential-drive UGV.
     //
-    // Parameters:
-    //   ticks_per_revolution = 1024
-    //   wheel_radius_m       = 0.3
-    //   wheelbase_m          = 1.0
+    //
+    // 
+     //Parameters:
+    constexpr int ticks_per_revolution = 1024; //імпульс за один оберт колеса
+    constexpr float wheel_radius_m  = 0.3; //радіус колеса у метрах (діаметр 60 см)
+    constexpr float wheelbase_m = 1.0; //відстань між лівим і правим бортом, у метрах
     //
     // Input:  text file with 5 whitespace-separated numbers per line:
     //         timestamp_ms fl_ticks fr_ticks bl_ticks br_ticks
+
+
     // Output: same tabular format on stdout, starting from the second sample:
     //         timestamp_ms x y theta
 
